0030f36209052918511486de5c2a6e2301d32c6e
Snehal Pansare <spansari@in.ibm.com>
2022-03-07 16:33:11 +0530

    Provision DV instance on CP4D
    Signed-off-by: Snehal Pansare <spansari@in.ibm.com>

--- test/stages/stage4-outputs.tf
+++ test/stages/stage4-outputs.tf
@@ -5,13 +5,13 @@ resource null_resource write_outputs {
 
     environment = {
       OUTPUT = jsonencode({
-        name        = module.gitops_module.name
-        branch      = module.gitops_module.branch
-        namespace   = module.gitops_module.namespace
-        server_name = module.gitops_module.server_name
-        layer       = module.gitops_module.layer
-        layer_dir   = module.gitops_module.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_module.layer == "services" ? "2-services" : "3-applications")
-        type        = module.gitops_module.type
+        name        = module.gitops_cp4d_dv_instance.name
+        branch      = module.gitops_cp4d_dv_instance.branch
+        namespace   = module.gitops_cp4d_dv_instance.namespace
+        server_name = module.gitops_cp4d_dv_instance.server_name
+        layer       = module.gitops_cp4d_dv_instance.layer
+        layer_dir   = module.gitops_cp4d_dv_instance.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_module.layer == "services" ? "2-services" : "3-applications")
+        type        = module.gitops_cp4d_dv_instance.type
       })
     }
   }
