5c7c8187d5cb75934366d80ba3a58ee2a5b66a6f
Snehal Pansare <spansari@in.ibm.com>
2022-03-10 12:13:48 +0530

    Provision DV instance on CP4D
    Signed-off-by: Snehal Pansare <spansari@in.ibm.com>

--- chart/provision-dv-instance/scripts/_update-configmap.sh
+++ chart/provision-dv-instance/scripts/_update-configmap.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
-SUPPORT_DIR=$(cd "${SCRIPT_DIR}/../support"; pwd -P)
-TEMPLATE_DIR=$(cd "${SCRIPT_DIR}/../templates"; pwd -P)
+SCRIPT_DIR=$(
+  cd $(dirname "$0")
+  pwd -P
+)
+SUPPORT_DIR=$(
+  cd "${SCRIPT_DIR}/../support"
+  pwd -P
+)
+TEMPLATE_DIR=$(
+  cd "${SCRIPT_DIR}/../templates"
+  pwd -P
+)
 
-cat "${SUPPORT_DIR}/configmap.snippet.yaml" > "${TEMPLATE_DIR}/configmap.yaml"
+cat "${SUPPORT_DIR}/configmap.snippet.yaml" >"${TEMPLATE_DIR}/configmap.yaml"
 
-oc create configmap dv-provision-cm \
-  --from-file=${SCRIPT_DIR}/create_dv_instance.sh \
-  --from-file=${SCRIPT_DIR}/ibm_dv_provisioner_role.yaml \
-  --from-file=${SCRIPT_DIR}/ibm_dv_provisioner_rolebinding.yaml \
-  --from-file=${SCRIPT_DIR}/newdv.json \
+oc create configmap tmp \
+  --from-file="${SCRIPT_DIR}"/create_dv_instance.sh \
+  --from-file="${SCRIPT_DIR}"/ibm_dv_provisioner_role.yaml \
+  --from-file="${SCRIPT_DIR}"/ibm_dv_provisioner_rolebinding.yaml \
+  --from-file="${SCRIPT_DIR}"/newdv.json \
   --dry-run=client \
-  -o yaml | \
-yq eval 'del(.apiVersion) | del(.kind) | del(.metadata)' - >> "${TEMPLATE_DIR}/configmap.yaml"
+  -o yaml |
+  yq eval 'del(.apiVersion) | del(.kind) | del(.metadata)' - >>"${TEMPLATE_DIR}/configmap.yaml"
