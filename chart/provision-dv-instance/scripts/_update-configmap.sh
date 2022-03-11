f96749521b574e59680c17fe0b839a97db4b4bde
Snehal Pansare <spansari@in.ibm.com>
2022-03-11 15:22:16 +0530

    Provision DV instance on CP4D
    Signed-off-by: Snehal Pansare <spansari@in.ibm.com>

--- chart/provision-dv-instance/scripts/_update-configmap.sh
+++ chart/provision-dv-instance/scripts/_update-configmap.sh
@@ -15,11 +15,11 @@ TEMPLATE_DIR=$(
 
 cat "${SUPPORT_DIR}/configmap.snippet.yaml" >"${TEMPLATE_DIR}/configmap.yaml"
 
-oc create configmap tmp \
+oc create configmap dv-provision-cm \
   --from-file="${SCRIPT_DIR}"/create_dv_instance.sh \
   --from-file="${SCRIPT_DIR}"/ibm_dv_provisioner_role.yaml \
   --from-file="${SCRIPT_DIR}"/ibm_dv_provisioner_rolebinding.yaml \
   --from-file="${SCRIPT_DIR}"/newdv.json \
   --dry-run=client \
   -o yaml |
-  yq eval 'del(.apiVersion) | del(.kind) | del(.metadata)' - >>"${TEMPLATE_DIR}/configmap.yaml"
+  yq eval 'del(.apiVersion) | del(.kind) | del(.metadata)' - >> "${TEMPLATE_DIR}/configmap.yaml"
