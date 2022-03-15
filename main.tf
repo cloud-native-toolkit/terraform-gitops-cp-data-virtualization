5b2c4cd4cbde26d928f738e2fd6ecdfe898aa40f
Snehal Pansare <spansari@in.ibm.com>
2022-03-15 22:49:01 +0530

    DV instance on CP4D
    Signed-off-by: Snehal Pansare <spansari@in.ibm.com>

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ module setup_service_account {
 
   gitops_config = var.gitops_config
   git_credentials = var.git_credentials
-  namespace = "default"
+  namespace = var.cpd_namespace
   name = var.service_account_name
   server_name = var.server_name  
 }
